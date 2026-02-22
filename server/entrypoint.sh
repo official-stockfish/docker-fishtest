9532fdba61db95ee9efc300adf061336d7d399c4
Disservin <disservin.social@gmail.com>
2026-02-22 12:07:05 +0100

    update config to latest fishtest

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -6,4 +6,4 @@ uv run utils/create_indexes.py actions flag_cache pgns runs users
 
 uv run /app/create_users.py
 
-uv run pserve development.ini --reload
+uv run uvicorn fishtest.app:app --host 0.0.0.0 --port 6542 --proxy-headers --forwarded-allow-ips=127.0.0.1 --limit-concurrency 10 --backlog 100 --log-level warning --workers 1 --reload
